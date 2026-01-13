85b25494f7298e9ffc9760cbba458651dd154c67
Opeyemi Oloyede <oloyedeope14@gmail.com>
2026-01-13 22:45:45 +0100

    fixed navbar and edited the styling

--- src/views/twoheads-home/page.tsx
+++ src/views/twoheads-home/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './components/navbar/page'
 
+
 export default function TwoHeads() {
   return (
     <div>
         <Navbar />
-        hello
     </div>
   )
 }
