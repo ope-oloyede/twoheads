4dd3a02c4e86445fbe04c9c8e3879769ba4b313b
Opeyemi Oloyede <oloyedeope14@gmail.com>
2026-01-14 15:38:37 +0100

    feat adds hero section

--- src/views/twoheads-home/page.tsx
+++ src/views/twoheads-home/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/page'
-
+import HeroSection from './components/hero-section/page'
 
 export default function TwoHeads() {
   return (
     <div>
         <Navbar />
+        <HeroSection />
     </div>
   )
 }
