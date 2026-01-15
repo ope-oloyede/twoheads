e3a54389394ea10c64df216a82f939341845ac9c
Opeyemi Oloyede <oloyedeope14@gmail.com>
2026-01-15 15:24:26 +0100

    adds hambugger to the mobile view and every other section

--- src/views/twoheads-home/components/footer/page.tsx
+++ src/views/twoheads-home/components/footer/page.tsx
@@ -5,20 +5,21 @@ import styles from "./styles.module.scss";
 
 export default function Footer() {
   return (
-    <div className="mx-auto px-100 py-4">
+    <div className="mx-auto px-6 py-8 md:px-25 md:py-12">
       <div className={styles.foot}>
-        <div className="max-w-5xl mt-15 mx-auto text-center">
-          <h1 className="text-5xl md:text-5xl font-bold text-black mb-12 leading-tight">
+        <div className="max-w-5xl mx-auto text-center">
+          <h1 className="text-3xl md:text-4xl font-bold text-black mb-8 md:mb-12 leading-tight">
             The new way to share your
             <br />
             knowledge with the world
           </h1>
+
           <a
             href="#"
             className={styles.cta}
             onClick={(e) => {
               e.preventDefault();
-              window.open("https://tally.so/r/mRyP2l", "_blank"); 
+              window.open("https://tally.so/r/mRyP2l", "_blank");
             }}
           >
             Join the waitlist
